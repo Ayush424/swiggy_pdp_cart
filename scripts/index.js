259a1623c550e105a5a3da62ba67ec2218c9f5f9
Ayush424 <ayush66.k@gmail.com>
2022-03-02 15:56:39 +0530

    fixed button

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-const body = document.body;
 // Arrays
 const categoriesListItems = [{ "displayName": "Recommended", "id": "recommended" }, { "displayName": "Dessert and Beverages", "id": "dessert_beverage" }, { "displayName": "Biryani", "id": "biryani" }];
 const menuListItems = [{ "id": "2121", "displayName": "Kadhai Paneer Biryani", "price": 249, "currency": "INR", "vegetarian": true, "imgUrl": "https://media.istockphoto.com/photos/fish-biryani-with-basmati-rice-indian-food-picture-id488481490?k=20&m=488481490&s=612x612&w=0&h=HYP2KxiC1e2tAtzmfrA7xxs3u8LD1wjSLPUD9bZ48eU=", "categories": ["recommended", "biryani"] }, { "id": "2122", "displayName": "Real Biryani", "price": 349, "currency": "INR", "vegetarian": false, "imgUrl": "https://media.istockphoto.com/photos/hyderabadi-biryani-a-popular-chicken-or-mutton-rice-preparation-picture-id466089615?k=20&m=466089615&s=612x612&w=0&h=_Z0Jlombq-VX8Pl8I9mJf_kIuvbzZ7j8ucxvRoLL8BM=", "categories": ["biryani"] }, { "id": "2123", "displayName": "Plain Veg Biryani", "price": 149, "currency": "INR", "vegetarian": true, "imgUrl": "https://media.istockphoto.com/photos/chicken-biryani-directly-above-photo-picture-id1169141170?k=20&m=1169141170&s=612x612&w=0&h=EpCF3lQF2GBRaVApNELuE5xFQfv8fyQ_wWC52hmyxeo=", "categories": ["biryani"] }];
@@ -62,9 +61,9 @@ const h3 = document.createElement("h3");
 h3.textContent = "Subtotal";
 const price = document.createElement("p");
 price.textContent = `₹ ${cartListItems.subTotal}`;
-const extraCharges = document.createElement("p");
-extraCharges.className = "extra-charges";
-extraCharges.textContent = "Extra charges may apply";
+const disclaimer = document.createElement("p");
+disclaimer.className = "disclaimer";
+disclaimer.textContent = "Extra charges may apply";
 const button = document.createElement("button");
 button.className = "check-out-button";
 button.innerText = "CHECKOUT ->";
@@ -72,6 +71,6 @@ cartDiv.appendChild(cartUl);
 amount.append(h3);
 amount.appendChild(price);
 cartDiv.appendChild(amount);
-cartDiv.append(extraCharges);
+cartDiv.append(disclaimer);
 cartDiv.append(button);
 
