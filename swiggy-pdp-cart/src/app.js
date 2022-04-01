486ec12be1cc146791845702093eda237fea8ea0
Ayush424 <ayush66.k@gmail.com>
2022-04-01 14:41:57 +0530

    React Router

--- swiggy-pdp-cart/src/app.js
+++ swiggy-pdp-cart/src/app.js
@@ -1,12 +1,18 @@
 import Header from "./header";
 import Main from "./main";
 import Footer from "./footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./homepage";
+
 export default function App() {
   return (
-      <>
+    <BrowserRouter>
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/restaurant/:name" element={<Main />} />
+      </Routes>
       <Footer />
-      </>
+    </BrowserRouter>
   );
 }
--- swiggy-pdp-cart/src/banner.js
+++ swiggy-pdp-cart/src/banner.js
@@ -1,11 +1,14 @@
 import React from "react";
+import chaap from "./images/chaap.jpg"
 import momos from "./images/momos.jpg"
 
-export default function Banner(){
+export default function Banner(props){
+    const heading =(props.restaurant==="PunjabiMomos")?'Punjabi Momos':'Punjabi Chaap'; 
+    const image=(props.restaurant==="PunjabiChaap")?chaap:momos;
     return(
         <div className="banner">
-            <img src={momos} alt="Momos"/>
-            <h1>Punjabi Momos</h1>
+            <img src={image} alt="Momos"/>
+            <h1>{heading}</h1>
         </div>
     );
 }
